8b40550cd3ff2dedb54e2c561bf198abdd202809
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-05-21 19:53:40 -0400

    added form partial

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,6 +1,17 @@
 class VoteController < ApplicationController::Base
 
   def create
+    vote = Vote.new(vote_params)
+    if vote.save!
+
+    else
+      render :new
+      flash[:warn] = 'Was not able to save this comment'
+    end
   end
+
+ def vote_parms
+    params.require(:vote).permit(:voteable_id, :voteable_type, :score).merge(user_id: current_user.id)
+ end
 
 end
