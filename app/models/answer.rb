227893f544d5e132f0a3adbcdf44646191ed8dc2
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-05-22 14:48:55 -0400

    labels the answer and user id

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,10 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   belongs_to :question
   has_many :votes, as: :voteable
+
+
+  # def karma
+  #   all_votes = Vote.where(voteable_id: id, voteable_type: "Question")
+  #   all_votes.sum(:score)
+  # end
 end
