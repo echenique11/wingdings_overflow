f54730480203a4a3647817219ef9158377d49ff5
Siddharth Watal <sid.watal@gmail.com>
2015-05-22 18:38:31 -0400

    fix validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,15 +2,15 @@ class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
   belongs_to :user
 
-  validates :score, :user_id, :voteable_type, :votable_id, presence: :true
-  validates_uniqueness_of :user_id, scope: [:votable_id, :voteable_type, :score]
+  validates :score, :user_id, :voteable_type, :voteable_id, presence: :true
+  validates_uniqueness_of :user_id, scope: [:voteable_id, :voteable_type, :score]
   validate :different_user
   validate :one_neg_one
 
   private
 
   def different_user
-    self.user != question.user
+    self.user != self.voteable.user
   end
 
   def one_neg_one
