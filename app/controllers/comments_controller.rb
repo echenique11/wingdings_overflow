11e44344b64e20497970363922fb93a356069245
Siddharth Watal <sid.watal@gmail.com>
2015-05-22 15:18:02 -0400

    make comment form usable on questions and answers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,16 @@ class CommentsController < ApplicationController
   before_filter :find_comment
 
   def create
-    question = Question.find_by(id: params[:question_id])
-    question.comments.build(body: params[:body], user_id: current_user.id)
-    if question.save
-      redirect_to :back
+    p params
+    if params[:answer_id]
+      commentable = Answer.find_by(id: params[:answer_id])
+    else
+      commentable = Question.find_by(id: params[:question_id])
+    end
+
+    commentable.comments.build(body: params[:body], user_id: current_user.id)
+    if commentable.save
+      redirect_to question_path(params[:question_id])
     else
       flash[:warning] = "That was an invalid comment."
       redirect_to :back
