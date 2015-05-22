7770d90b8e140aa1030167f4f84b32d9d6106f44
Siddharth Watal <sid.watal@gmail.com>
2015-05-22 14:13:08 -0400

    fix comment routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     resources :answers, only: [:create, :edit, :update]
   end
 
-  resources :comments, except: [:index, :show]
+  resources :comments, except: [:index, :show, :new]
   resources :votes, only: [:create]
   resources :tags, only: [:show, :index]
 
