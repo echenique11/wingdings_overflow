deb76c10b0d3c2d85ae5c6a309c51d760854d4c6
Siddharth Watal <sid.watal@gmail.com>
2015-05-21 20:50:25 -0400

    finished and ran migrations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,43 @@
 class CommentsController < ApplicationController
   before_filter :find_comment
 
+  def new
+  end
 
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to comment_path(@comment)
+    else
+      flash[:error] = "That was an invalid comment."
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @comment.update_attributes(params[:comment])
+      flash[:success] = "Comment updated."
+      redirect_to question_path(@comment)
+    else
+      flash[:error] = "That was an invalid comment."
+      render :edit
+    end
+  end
+
+  def destroy
+    @comment.delete
+    redirect_to :back
+  end
 
   private
   def find_comment
-    @model = Comment.find_by(id: params[:id]) if params[:id]
+    @comment = Comment.find_by(id: params[:id]) if params[:id]
+  end
+
+  def comment_params
+    params.require(:comment).permit(:title, :body).merge(user_id: current_user.id )
   end
 end
