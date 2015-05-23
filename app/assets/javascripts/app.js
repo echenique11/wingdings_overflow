34ccf5c505ed6b8fb8e9f2723d1dcad1b3d0ee37
Siddharth Watal <sid.watal@gmail.com>
2015-05-23 17:22:31 -0400

    AJAX on question comments

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,7 @@
 $( document ).ready(function() {
   $('.toggle-comment-form').on('click', toggleComment);
   $('.vote-button').on('submit', createVote);
+  $('.comment-form').on('submit', createComment);
 });
 
 var toggleComment = function(event) {
@@ -9,19 +10,27 @@ var toggleComment = function(event) {
 };
 
 var createVote = function(event){
-
   event.preventDefault();
-
-  var formUrl = event.target.action;
-  var formMethod = event.target.method;
-  var formParams = $(event.target).serialize();
-
   $.ajax({
-    url: formUrl,
-    method: formMethod,
-    data: formParams,
+    url: event.target.action,
+    method: event.target.method,
+    data: $(event.target).serialize(),
   }).done(function(response){
     $(event.target).parent().find('.karma').text(response);
+  }).fail(function(error){
+    console.log(error);
+  });
+};
+
+var createComment = function(event){
+  event.preventDefault();
+  $.ajax({
+    url: event.target.action,
+    method: event.target.method,
+    data: $(event.target).serialize(),
+  }).done(function(response){
+    $('.comments').append(response);
+    $('#comment_body').val("");
   }).fail(function(error){
     console.log(error);
   });
