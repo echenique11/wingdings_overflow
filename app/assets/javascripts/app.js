42d056b3b11eda411e40eb14c49ca48410dc4a0e
Siddharth Watal <sid.watal@gmail.com>
2015-05-23 15:43:16 -0400

    added ajax and jquery to votes

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,8 +1,29 @@
 $( document ).ready(function() {
   $('.toggle-comment-form').on('click', toggleComment);
+  $('.vote-button').on('submit', createVote);
 });
 
 var toggleComment = function(event) {
   event.preventDefault();
   $('.comment-form').toggle();
+};
+
+var createVote = function(event){
+
+  event.preventDefault();
+
+  var formUrl = event.target.action;
+  var formMethod = event.target.method;
+  var formParams = $(event.target).serialize();
+
+  $.ajax({
+    url: formUrl,
+    method: formMethod,
+    data: formParams,
+  }).done(function(response){
+    console.log($(event.target).parent());
+    $(event.target).parent().find('.karma').text(response);
+  }).fail(function(error){
+    console.log(error);
+  });
 };
