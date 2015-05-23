5b6a3e0a492982e026f259e0d997e5f4df46da9e
Doraly Pantaleon <doralypantaleon@gmail.com>
2015-05-23 14:17:00 -0400

    adds gravatar to profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
 	def gravitar(email)
-	  hash = Digest::MD5.hexdigest(email.strip.downcase)
-	  "<img id='profile-photo' src='http://www.gravatar.com/avatar/#{hash}'>"
+	  Digest::MD5.hexdigest(email.strip.downcase)
 	end
 end
