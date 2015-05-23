23218bc3cd67e8f090543a89bbd482bfe37f6926
echenique11 <echenique11@gmail.com>
2015-05-23 11:42:20 -0400

    search by tags

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,15 @@ class User < ActiveRecord::Base
 	has_many :comments
 
 	def questions_by(key, value)
-			questions.where(["#{key} = ? ",value])
+		if(value)
+			p "value #{value}"
+			return questions.eager_load(:tags).where(["tags.tag_name LIKE ?","%#{value}%"]) if key == "tags"
+			questions.where([" title LIKE ?","%#{value}%"])
+		else 
+			p "no value"
+			p questions
+			questions
+		end
 	end
 
   validates :username, :email, :password, presence: :true
